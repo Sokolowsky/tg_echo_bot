532e81b2b596911ba18e5caa2fd2883e0d391097
flasze <nsgorlat@gmail.com>
2024-03-22 22:29:36 +1000

    1.0

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-br = 123
+import requests
+import time
+
+API_URL = 'https://api.telegram.org/bot'
+BOT_TOKEN = '6823518119:AAE4u7gj684utCAtHKxT_Agi6pnhTLQOnw4'
+MAX_COUNTER = 100
+
+offset = -2
+counter = 0
+chat_id: int
+
+while counter < MAX_COUNTER:
+
+    print('attempt =', counter)  # Чтобы видеть в консоли, что код живет
+
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset + 1}').json()
+    print(updates)
+    if updates['result']:
+        for result in updates['result']:
+            offset = result['update_id']
+            chat_id = result['message']['from']['id']
+            requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo=https://prnt.sc/1MzUIJ37qElw&caption=это моя тетя')
+            requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo=https://prnt.sc/N2cFuRt7lnOu&caption=это моя тетя')
+
+    time.sleep(1)
+    counter += 1
