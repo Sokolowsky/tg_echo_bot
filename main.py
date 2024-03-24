4734f3c80ad779fd96a7f09cb24316591ac71e66
flasze <nsgorlat@gmail.com>
2024-03-25 00:45:42 +1000

    final

--- main.py
+++ main.py
@@ -1,38 +1,30 @@
-from aiogram import Bot, Dispatcher, F
+from aiogram import Bot, Dispatcher
 from aiogram.filters import Command
 from aiogram.types import Message
 
-BOT_TOKEN = 'blablabla'
+BOT_TOKEN = 'paste here'
+
+#Bot and dispatcher classes
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
-#Handler for /start
+#/Start handler
+@dp.message(Command(commands='start'))
 async def process_start_command(message: Message):
-    await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
+    await message.answer('Привет, я тупой слаборазвитый бот, который все что умеет, это повторять!')
 
-#Handler for /help
+#/Help handler
+@dp.message(Command(commands='help'))
 async def process_help_command(message: Message):
-    await message.answer(
-        'Напиши мне что-нибудь и в ответ '
-        'я пришлю тебе твое сообщение')
-async def process_photo_command(message: Message):
-    await message.answer_photo(message.photo[-1].file_id)
+    await message.answer('Напиши мне что-нибудь!')
 
-async def process_sticker_command(message: Message):
-    await message.answer_sticker(message.sticker.file_id)
-
-async def process_voice_command(message: Message):
-    await message.answer_voice(message.voice.file_id)
-
+#Handler for everything expect help/start
+@dp.message()
 async def send_echo(message: Message):
-    await message.answer(text=message.text)
-
-dp.message.register(process_start_command, Command(commands='start'))
-dp.message.register(process_help_command, Command(commands='help'))
-dp.message.register(process_photo_command, F.photo)
-dp.message.register(process_sticker_command, F.sticker)
-dp.message.register(process_voice_command, F.voice)
-dp.message.register(send_echo)
+    try:
+        await message.send_copy(chat_id=message.chat.id)
+    except TypeError:
+        await message.reply(text='Я ваще не пойму че это')
 
 if __name__ == '__main__':
     dp.run_polling(bot)
