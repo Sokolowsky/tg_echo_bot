24b6e2b469d2d3a89dedb3db00fd3b97f88e47cd
flasze <nsgorlat@gmail.com>
2024-03-25 00:27:22 +1000

    1.02

--- main.py
+++ main.py
@@ -1,35 +1,38 @@
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
 from aiogram.types import Message
 
-# Вместо BOT TOKEN HERE нужно вставить токен вашего бота, полученный у @BotFather
 BOT_TOKEN = 'blablabla'
-
-# Создаем объекты бота и диспетчера
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
-
-# Этот хэндлер будет срабатывать на команду "/start"
-@dp.message(Command(commands=["start"]))
+#Handler for /start
 async def process_start_command(message: Message):
     await message.answer('Привет!\nМеня зовут Эхо-бот!\nНапиши мне что-нибудь')
 
-# Этот хэндлер будет срабатывать на команду "/help"
-@dp.message(Command(commands=['help']))
+#Handler for /help
 async def process_help_command(message: Message):
     await message.answer(
         'Напиши мне что-нибудь и в ответ '
-        'я пришлю тебе твое сообщение'
-    )
+        'я пришлю тебе твое сообщение')
+async def process_photo_command(message: Message):
+    await message.answer_photo(message.photo[-1].file_id)
 
+async def process_sticker_command(message: Message):
+    await message.answer_sticker(message.sticker.file_id)
 
-# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
-# кроме команд "/start" и "/help"
-@dp.message()
+async def process_voice_command(message: Message):
+    await message.answer_voice(message.voice.file_id)
+
 async def send_echo(message: Message):
-    await message.reply(text=message.text)
+    await message.answer(text=message.text)
 
+dp.message.register(process_start_command, Command(commands='start'))
+dp.message.register(process_help_command, Command(commands='help'))
+dp.message.register(process_photo_command, F.photo)
+dp.message.register(process_sticker_command, F.sticker)
+dp.message.register(process_voice_command, F.voice)
+dp.message.register(send_echo)
 
 if __name__ == '__main__':
     dp.run_polling(bot)
